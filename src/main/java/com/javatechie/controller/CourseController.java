15c42341708e4fdf3e8024fb57105ce674296b5d
Ashish Singh <dexterousashish@users.noreply.github.com>
2024-07-13 18:49:17 +0530

    Update CourseController.java

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -62,10 +62,10 @@ public class CourseController {
 	 */
 
 
-    @GetMapping("/welcome")
-    public String greetings() {
-        return "Hello Techie , AWS CICD Example working fine !";
-    }
+	/*
+	 * @GetMapping("/welcome") public String greetings() { return
+	 * "Hello Techie , AWS CICD Example working fine !"; }
+	 */
 
 
 }
